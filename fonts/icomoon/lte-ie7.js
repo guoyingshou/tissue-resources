b48cf131301cb5097d46cb4dc71ecd0beb1cc11a
guoyingshou <guoyingshou@tianji.com>
2013-01-28 13:13:05 +0800

    added iconfont support

---
+++ fonts/icomoon/lte-ie7.js
@@ -0,0 +1,37 @@
+/* Use this script if you need to support IE 7 and IE 6. */
+
+window.onload = function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+			'icon-user' : '&#xe005;',
+			'icon-user-2' : '&#xe002;',
+			'icon-pencil' : '&#xe000;',
+			'icon-users' : '&#xe001;',
+			'icon-cog' : '&#xe003;',
+			'icon-trashcan' : '&#xe004;',
+			'icon-time' : '&#xe006;',
+			'icon-plus' : '&#xe007;',
+			'icon-minus' : '&#xe008;',
+			'icon-envelope' : '&#xf003;',
+			'icon-question' : '&#xe009;',
+			'icon-loop' : '&#xe01c;',
+			'icon-list' : '&#xe00a;'
+		},
+		els = document.getElementsByTagName('*'),
+		i, attr, html, c, el;
+	for (i = 0; i < els.length; i += 1) {
+		el = els[i];
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+};
